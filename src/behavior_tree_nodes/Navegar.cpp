cb22f1fdc2b1134e079500e1b5c4529f5068cf5b
Jorge Rando <jorgerando2000@hotmail.com>
2022-04-30 15:15:50 +0100

    Nodo Navecion Actualizados y Funcionando en el tree/similador

--- src/behavior_tree_nodes/Navegar.cpp
+++ src/behavior_tree_nodes/Navegar.cpp
@@ -13,14 +13,12 @@
 // limitations under the License.
 
 #include <string>
-
 #include "behavior_tree/Navegar.h"
 #include "geometry_msgs/Twist.h"
 #include "std_msgs/Bool.h"
 #include "geometry_msgs/PoseStamped.h"
-
 #include "move_base_msgs/MoveBaseActionResult.h"
-
+#include "std_msgs/Int64.h"
 #include "behaviortree_cpp_v3/behavior_tree.h"
 #include "std_msgs/String.h"
 #include "ros/ros.h"
@@ -29,35 +27,18 @@
 namespace behavior_trees
 {
 
-Navegar::Navegar(const std::string& name , const BT::NodeConfiguration & config): BT::ActionNodeBase(name, config),nh_(),feedBack(" ")
+Navegar::Navegar(const std::string& name , const BT::NodeConfiguration & config): BT::ActionNodeBase(name, config),nh_(),feedBack(-100)
 { 
-   
-  activador = nh_.advertise<geometry_msgs::PoseStamped>("move_base_simple/goal",10);
-
-  sub = nh_.subscribe("/move_base/result", 10, &Navegar::messageCallback, this);
-
-
+  int fr = 10 ;
+  activador = nh_.advertise<geometry_msgs::PoseStamped>("/navigate_to",fr);
+  sub = nh_.subscribe("/resultado_navegacion", fr, &Navegar::messageCallback, this);
 }
 
 void
-Navegar::messageCallback(const move_base_msgs::MoveBaseActionResult::ConstPtr& msg)
+Navegar::messageCallback(const std_msgs::Int64::ConstPtr& msg_)
 {
-  feedBack = msg->status.text ;
-  std::cout << "Resultado Navegacion : " << feedBack << "\n";
+  feedBack = msg_->data ;
 }
-
-
-
-
-  
-
-
-}
-
-
-
-
-
 
 void
 Navegar::halt()
@@ -69,18 +50,18 @@ BT::NodeStatus
 Navegar::tick()
 {
 
+  if (ac == 5) 
+  {
 
-  if (a == 5) {
+    ROS_INFO(" POSICION ARBITRO COMUNICADA ");
 
-    std::cout << a << "\n";
-    
     geometry_msgs::PoseStamped msg;
-
+    
+    // donde estara el arbitro 
     msg.header.stamp = i;
     msg.header.frame_id = "map";
 
-
-    msg.pose.position.x = 0.0;
+    msg.pose.position.x = -5.0;
     msg.pose.position.y = 0.0;
     msg.pose.position.z = 0.0;
 
@@ -89,24 +70,27 @@ Navegar::tick()
     msg.pose.orientation.z = 0.0;
     msg.pose.orientation.w = 1.0;
 
-
     activador.publish(msg);
   }
-  a++;
-
-
-  if (feedBack != "") {
-
-    if(feedBack == "Goal reached."){
-      return BT::NodeStatus::SUCCESS;
-    }else{
-      return BT::NodeStatus::FAILURE;
-    }
+  ac++;
+  std::cout << "Feedback : " <<  feedBack ;
   
-  }  
+  if(feedBack == 1 )
+  {
+      
+    ROS_INFO(" ARBITRO ALCANSADO ");
+    return BT::NodeStatus::SUCCESS;
 
+  }else if(feedBack == -1 ) 
+  {
+
+    ROS_INFO(" FALLO EN LA NAVEGACION ");
+    return BT::NodeStatus::FAILURE;
+  }
+    
+  ROS_INFO(" LLEGANDO A LA POSICION ARBITRO ");
   return BT::NodeStatus::RUNNING;
-
+ 
 }
 
 }  // namespace behavior_trees
--- src/behavior_tree_nodes/Navegar2.cpp
+++ src/behavior_tree_nodes/Navegar2.cpp
@@ -1,24 +1,10 @@
-// Copyright 2019 Intelligent Robotics Lab
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 #include <string>
-
 #include "behavior_tree/Navegar2.h"
 #include "geometry_msgs/Twist.h"
 #include "std_msgs/Bool.h"
 #include "geometry_msgs/PoseStamped.h"
 #include "move_base_msgs/MoveBaseActionResult.h"
+#include "std_msgs/Int64.h"
 #include "behaviortree_cpp_v3/behavior_tree.h"
 #include "std_msgs/String.h"
 #include "ros/ros.h"
@@ -27,22 +13,18 @@
 namespace behavior_trees
 {
 
-Navegar2::Navegar2(const std::string& name , const BT::NodeConfiguration & config): BT::ActionNodeBase(name, config),nh_(),feedBack(" ")
+Navegar2::Navegar2(const std::string& name , const BT::NodeConfiguration & config): BT::ActionNodeBase(name, config),nh_(),feedBack(-100)
 { 
-   
-  activador = nh_.advertise<geometry_msgs::PoseStamped>("move_base_simple/goal",10);
-  sub = nh_.subscribe("/move_base/result", 10, &Navegar2::messageCallback, this);
-
-
+  int fr = 10 ;
+  activador = nh_.advertise<geometry_msgs::PoseStamped>("/navigate_to",fr);
+  sub = nh_.subscribe("/resultado_navegacion", fr, &Navegar2::messageCallback, this);
 }
 
 void
-Navegar2::messageCallback(const move_base_msgs::MoveBaseActionResult::ConstPtr& msg)
+Navegar2::messageCallback(const std_msgs::Int64::ConstPtr& msg_)
 {
-  feedBack = msg->status.text ;
-  std::cout << "Resultado Navegacion : " << feedBack << "\n";
+  feedBack = msg_->data ;
 }
-
 
 void
 Navegar2::halt()
@@ -54,12 +36,14 @@ BT::NodeStatus
 Navegar2::tick()
 {
 
-  
-  if (a == 5) {
-    std::cout << a << "\n";
-    
-    geometry_msgs::PoseStamped msg;
+  if (ac == 5) 
+  {
 
+    ROS_INFO(" VUELTA AL ORIGEN ACTIVADA ");
+
+    geometry_msgs::PoseStamped msg;
+    
+    // donde estara el arbitro 
     msg.header.stamp = i;
     msg.header.frame_id = "map";
 
@@ -72,22 +56,27 @@ Navegar2::tick()
     msg.pose.orientation.z = 0.0;
     msg.pose.orientation.w = 1.0;
 
-
     activador.publish(msg);
   }
-  a++;
-
-  if (feedBack != "") {
-
-    if(feedBack == "Goal reached."){
-      return BT::NodeStatus::SUCCESS;
-    }else{
-      return BT::NodeStatus::FAILURE;
-    }
+  ac++;
+  std::cout << "Feedback : " <<  feedBack ;
   
-  }  
+  if(feedBack == 1 )
+  {
+      
+    ROS_INFO(" ORIGEN ALCANSADO ");
+    return BT::NodeStatus::SUCCESS;
 
+  }else if(feedBack == -1 ) 
+  {
+
+    ROS_INFO(" FALLO EN LA NAVEGACION ");
+    return BT::NodeStatus::FAILURE;
+  }
+    
+  ROS_INFO(" LLEGANDO AL ORIGEN ");
   return BT::NodeStatus::RUNNING;
+ 
 }
 
 }  // namespace behavior_trees
