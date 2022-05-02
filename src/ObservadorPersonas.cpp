d6b03d3ea389d0ee8be756fb9e75baa89790aa42
mrubiop2019 <m.rubiop.2019@alumnos.urjc.es>
2022-05-03 00:23:39 +0200

    nodos observador y recopilador de datos

--- src/ObservadorPersonas.cpp
+++ src/ObservadorPersonas.cpp
@@ -9,62 +9,159 @@
 #include <message_filters/subscriber.h>
 #include <message_filters/time_synchronizer.h>
 #include <message_filters/sync_policies/approximate_time.h>
+#include "std_msgs/String.h"
+#include "std_msgs/Bool.h"
+
 
 #include <memory>
 
-cv_bridge::CvImagePtr mImageData;
+cv_bridge::CvImagePtr DepthImageData;
+cv_bridge::CvImagePtr RgbImageData;
 ros::Publisher mSensorsPublisher;
+ros::Subscriber Activador;
+ros::Publisher treePub;
+ros::Publisher objectPub;
+u_int R_ = 0;
+u_int G_ = 0;
+u_int B_ = 0;
 float min_dist;
-bool found;
+bool found_person;
+bool found_object;
+bool act = false;
 double now;
 int fr = 10;
 geometry_msgs::Pose2D pp;
+std_msgs::String msg;
+std_msgs::String object;
+float xmax;
+float xmin;
+float ymax;
+float ymin;
+
+void activacionTree(const std_msgs::Bool::ConstPtr& clave)
+{ 
+  act = clave->data;
+  ROS_INFO("Observador Activado");
+
+}
 
 
+void callback_bbx(const sensor_msgs::ImageConstPtr& depth, const sensor_msgs::ImageConstPtr& rgb, const darknet_ros_msgs::BoundingBoxesConstPtr& boxes)
+{
+	if(act){
+		DepthImageData = cv_bridge::toCvCopy(*depth, sensor_msgs::image_encodings::TYPE_32FC1);
+		cv::Mat profundidadG_;
+		/////////////////////
+		//cv::GaussianBlur(mImageData->image,profundidadG_,cv::Size(33,33),0,0,0);
+		
+		cv::erode(DepthImageData->image, profundidadG_, cv::Mat());
+		//////////////////////******************CAMBIOS*****************
 
-void callback_bbx(const sensor_msgs::ImageConstPtr& img, const darknet_ros_msgs::BoundingBoxesConstPtr& boxes){
+		RgbImageData = cv_bridge::toCvCopy(*rgb, sensor_msgs::image_encodings::RGB16);
+		//cv::Mat rgb = RgbImageData->image;
+		//cv::erode(RgbImageData->image, rgb, cv::Mat());
 
-	mImageData = cv_bridge::toCvCopy(*img, sensor_msgs::image_encodings::TYPE_32FC1);
-	cv::Mat profundidadG_ ;
-	/////////////////////
-    //cv::GaussianBlur(mImageData->image,profundidadG_,cv::Size(33,33),0,0,0);
-	
-	cv::erode(mImageData->image, profundidadG_, cv::Mat());
-	//////////////////////******************CAMBIOS*****************
-	std::string tag = "person";
-	
-	float px_center=mImageData->image.cols/2;
-	float py_center=mImageData->image.rows/2;
-	// #############################
-	//min_dist=3;
-	min_dist=5;
-	// #####################################
-	found = false;
-	
-	for(int i=0;i<boxes->bounding_boxes.size();i++)
-	{
-		if (boxes->bounding_boxes[i].Class == tag)
+		std::string tag = "sports ball";
+		
+		float px_center=DepthImageData->image.cols/2;
+		float py_center=DepthImageData->image.rows/2;
+		// #############################
+		//min_dist=3;
+		min_dist=0.5;
+		// #####################################
+		found_person = false;
+		found_object = false;
+		
+		for(int i=0;i<boxes->bounding_boxes.size();i++)
 		{
-			float px = (boxes->bounding_boxes[i].xmax + boxes->bounding_boxes[i].xmin) / 2;
-			float py = (boxes->bounding_boxes[i].ymax + boxes->bounding_boxes[i].ymin) / 2;
-
-			float dist = profundidadG_.at<float>(cv::Point(px, py)) * 0.001f;
-
-			if (dist < min_dist)
+			if (boxes->bounding_boxes[i].Class == tag)
 			{
-				min_dist = dist;
-				pp.x = dist;
-				float angle = (px - px_center)/(px_center);
-				pp.y = angle;
-				found = true;
+				float px = (boxes->bounding_boxes[i].xmax + boxes->bounding_boxes[i].xmin) / 2;
+				float py = 2 * (boxes->bounding_boxes[i].ymax + boxes->bounding_boxes[i].ymin) / 3;
+				
+
+				float dist = profundidadG_.at<float>(cv::Point(px, py)) * 0.001f;
+
+
+				if (dist < min_dist)
+				{
+					min_dist = dist;
+					//pp.x = dist;
+					//float angle = (px - px_center)/(px_center);
+					//pp.y = angle;
+					cv::Vec3b vector_rgb = RgbImageData->image.at<cv::Vec3b>(cv::Point(px,py));
+
+					R_ = vector_rgb[2];
+					G_ = vector_rgb[1];
+					B_ = vector_rgb[0];
+
+					pp.x = R_;
+					pp.y = G_;
+					pp.theta = B_;
+
+					xmax = boxes->bounding_boxes[i].xmax;
+					xmin = boxes->bounding_boxes[i].xmin;
+					ymax = boxes->bounding_boxes[i].ymax;
+					ymin = boxes->bounding_boxes[i].ymin;
+
+
+					if(pp.x != 0 || pp.y != 0 || pp.theta != 0){
+						found_person = true;
+					}
+
+					}
+			}
+		}
+		
+
+		if (found_person)
+		{
+			for(int i=0;i<boxes->bounding_boxes.size();i++)
+			{
+				float px = (boxes->bounding_boxes[i].xmax + boxes->bounding_boxes[i].xmin) / 2;
+				float py = (boxes->bounding_boxes[i].ymax + boxes->bounding_boxes[i].ymin) / 2;
+
+				float s1x = xmax - xmin;
+				float s2x = boxes->bounding_boxes[i].xmax - boxes->bounding_boxes[i].xmin;
+
+				float s1y = ymax - ymin;
+				float s2y = boxes->bounding_boxes[i].ymax - boxes->bounding_boxes[i].ymin;
+
+
+				if( (xmin < px < xmax) && (ymin < py < ymax) && (s1x > s2x) && (s1y > s2y) )
+				{
+					std::stringstream ss;
+					ss << boxes->bounding_boxes[i].Class;
+					object.data = ss.str();
+					objectPub.publish(object);
+
+					std::stringstream ss2;
+					ss2 << "SUCCESS";
+					msg.data = ss2.str();
+					treePub.publish(msg);
+
+					mSensorsPublisher.publish(pp);
+					found_object = true;
 				}
+			}
+			if(!found_object)
+			{
+			std::stringstream ss;
+			ss << "RUNNING";
+			msg.data = ss.str();
+			treePub.publish(msg);
+			//Solo para tests
+			mSensorsPublisher.publish(pp);
+			}
+		}
+		else
+		{
+			std::stringstream ss;
+			ss << "FAILURE";
+			msg.data = ss.str();
+			treePub.publish(msg);
 		}
 	}
-
-	if (found){
-		ROS_INFO("PERSON DETECTED AT [%f],[%f]", pp.x, pp.y);
-		mSensorsPublisher.publish(pp);
-		}
 }
 
 int main(int argc, char** argv)
@@ -75,14 +172,19 @@ int main(int argc, char** argv)
 	ros::NodeHandle nh;
 
 	message_filters::Subscriber<sensor_msgs::Image> image_depth_sub(nh, "/camera/depth/image_raw", fr);
+	message_filters::Subscriber<sensor_msgs::Image> image_rgb_sub(nh, "/camera/rgb/image_raw", fr);
   	message_filters::Subscriber<darknet_ros_msgs::BoundingBoxes> bbx_sub(nh, "/darknet_ros/bounding_boxes", fr);
+	
 
- 	typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Image, darknet_ros_msgs::BoundingBoxes> Sync;
-  	message_filters::Synchronizer<Sync> sync_bbx(Sync(10), image_depth_sub, bbx_sub);
+ 	typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Image, sensor_msgs::Image, darknet_ros_msgs::BoundingBoxes> Sync;
+  	message_filters::Synchronizer<Sync> sync_bbx(Sync(10), image_depth_sub, image_rgb_sub, bbx_sub);
 
-  	sync_bbx.registerCallback(boost::bind(&callback_bbx, _1, _2));
+  	sync_bbx.registerCallback(boost::bind(&callback_bbx, _1, _2, _3));
 
 	mSensorsPublisher = nh.advertise<geometry_msgs::Pose2D>("/person_data", fr);
+	Activador = nh.subscribe("/control_observador", fr, activacionTree);
+	treePub = nh.advertise<std_msgs::String>("/status_observador", fr);
+	objectPub = nh.advertise<std_msgs::String>("/object_data", fr);
 
 	ros::spin();
 	
---
+++ src/data_collector.cpp
@@ -0,0 +1,210 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include "std_msgs/Bool.h"
+#include "geometry_msgs/Pose2D.h"
+#include <math.h>
+#include <string>
+#include <iostream>
+
+int fr =  10;
+bool act;
+geometry_msgs::Pose2D cSensorsData;
+std_msgs::String nSensorsData;
+std_msgs::String oSensorsData;
+std_msgs::String msg;
+ros::Publisher talkPub;
+ros::Publisher treePub;
+
+struct Person {
+
+    std::string color;
+    std::string name;
+    std::string object;
+    
+};
+
+struct RGB {
+
+    int r;
+    int g;
+    int b;
+    std::string name;
+
+};
+
+Person people[3];
+int people_counter = 0;
+//Person persona;
+
+RGB violet = {
+    148,
+    0,
+    211,
+    "VIOLET",
+};
+
+RGB purple = {
+    75,
+    0,
+    130,
+    "PURPLE",
+};
+
+RGB blue = {
+    0,
+    0,
+    255,
+    "BLUE",
+};
+
+RGB green = {
+    0,
+    255,
+    0,
+    "GREEN",
+};
+
+RGB yellow = {
+    255,
+    255,
+    0,
+    "YELLOW",
+};
+
+RGB orange = {
+    255,
+    127,
+    0,
+    "ORANGE",
+};
+
+RGB red = {
+    255,
+    0,
+    0,
+    "RED",
+};
+
+RGB black = {
+    0,
+    0,
+    0,
+    "BLACK",
+};
+
+RGB white = {
+    255,
+    255,
+    255,
+    "WHITE",  
+};
+
+RGB rainbow[] = {violet,purple,blue,green,yellow,orange,red,black,white};
+double ColourDistance(RGB e1, RGB e2)
+{
+  long rmean = ( (long)e1.r + (long)e2.r ) / 2;
+  long r = (long)e1.r - (long)e2.r;
+  long g = (long)e1.g - (long)e2.g;
+  long b = (long)e1.b - (long)e2.b;
+  return sqrt((((512+rmean)*r*r)>>8) + 4*g*g + (((767-rmean)*b*b)>>8));
+}
+
+std::string getColor(RGB valores){
+    double d_min;
+    int result;
+    for(int i = 0; i < 9; i++)
+    {
+        if (( i == 0) || (ColourDistance(valores, rainbow[i]) < d_min) )
+        {
+            d_min = ColourDistance(valores, rainbow[i]);
+            result = i;
+            }
+        //ROS_INFO("%d", d_min);
+    }
+    //ROS_INFO("%d", result);
+    //ROS_INFO("%s", rainbow[result].name);
+    std::cout << rainbow[result].name << std::endl;
+    return rainbow[result].name;
+
+}
+
+void personReceived(const geometry_msgs::Pose2DConstPtr& rgb_)
+{
+	cSensorsData = *rgb_;
+    //persona = people[people_counter];
+
+    RGB color ={
+        cSensorsData.x,
+        cSensorsData.y,
+        cSensorsData.theta,
+        "unkown",
+    };
+    people[people_counter].color = getColor(color);
+}
+
+void objectReceived(const std_msgs::String::ConstPtr& object_)
+{
+	oSensorsData = *object_;
+    people[people_counter].object = oSensorsData.data;
+}
+
+void voiceReceived(const std_msgs::String::ConstPtr& name_)
+{
+	nSensorsData = *name_;
+    people[people_counter].name = nSensorsData.data;
+    people_counter++;
+}
+
+void talk(ros::Publisher talkPub, Person people[], int people_counter)
+{
+    std_msgs::String msg;
+    for (int i = 0; i < people_counter; i++){
+        std::stringstream ss;
+        ss << people[i].name << " is wearing a " << people[i].color << " shirt and is holding a " << people[i].object;
+        msg.data = ss.str();
+	    talkPub.publish(msg);
+        sleep(100);
+    }
+}
+
+void activacionTree(const std_msgs::Bool::ConstPtr& pp)
+{ 
+  act = pp->data  ;
+  ROS_INFO("HABLANDO...");
+  if(act){
+      talk(talkPub, people, people_counter);
+      std::stringstream ss;
+	  ss << "SUCCESS";
+	  msg.data = ss.str();
+	  treePub.publish(msg);
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+int main(int argc, char** argv)
+{
+
+	ros::init(argc, argv, "data_collector");
+	ros::NodeHandle nh;
+	ros::Rate loop_rate(fr);
+
+    //ros::Publisher cPub = nh.advertise<geometry_msgs::Pose2D> ("/controller_instructions", fr, true);
+	talkPub = nh.advertise<std_msgs::String>("/nodo_hablar", fr);
+	treePub = nh.advertise<std_msgs::String>("/status_data", fr);
+
+	ros::Subscriber colorSub = nh.subscribe<geometry_msgs::Pose2D>("/person_data", fr, personReceived);
+	ros::Subscriber listenerSub = nh.subscribe<std_msgs::String>("/listener_data", fr, voiceReceived);
+    ros::Subscriber objectSub = nh.subscribe<std_msgs::String>("/object_data", fr, objectReceived);
+	ros::Subscriber Activador = nh.subscribe("/control_data", fr, activacionTree);
+
+    ros::spin();
+}
